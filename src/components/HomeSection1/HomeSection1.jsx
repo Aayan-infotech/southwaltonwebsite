16cbf78dc09577c6b12aa141d38650187deb3ee7
unknown <akashtripathi2002@gmail.com>
2024-10-14 17:32:17 +0530

    ag

--- src/components/HomeSection1/HomeSection1.jsx
+++ src/components/HomeSection1/HomeSection1.jsx
@@ -1,18 +1,28 @@
 import React from 'react';
 import './HomeSection1.scss';
 import homebg2 from "./img/homebg2.png";
-import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 const HomeSection1 = () => {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    const userId = localStorage.getItem('user'); // Adjust the key if it's different
+
+    if (userId) {
+      navigate('/home3'); // Navigate to home3 if userId exists
+    } else {
+      navigate('/login'); // Navigate to login if userId does not exist
+    }
+  };
+
   return (
     <div className='home1'>
       <div className="image-container">
         <img src={homebg2} alt="Background" className="background-image" />
         <div className="home1-btn">
-          <button>
-            <Link to ='/home3' style={{textDecoration:"none", color:"black"}}>
-              Reserve A Cart
-            </Link>
+          <button onClick={handleClick}>
+            Reserve A Cart
           </button>
         </div>
       </div>
