ec05035fb0dc14d583d82c70282be8d7c731bbf0
Atul Rajput <92659293+AtulRajput01@users.noreply.github.com>
2024-11-15 18:09:19 +0530

    Update Payment.jsx

--- src/components/Payment/Payment.jsx
+++ src/components/Payment/Payment.jsx
@@ -1270,7 +1270,7 @@ import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 import './Payment.scss';
 
-const stripePromise = loadStripe('pk_test_51PsifGP6k3IQ77YBgYy0dyPsIGz96iJFxWPDYwRNuEVxynkuKT6XfKeL73g5rqn6cf7LVApW2GUat2Zfjk7HCKwZ005FCFTkvr');
+const stripePromise = loadStripe('pk_test_51PsifGP6k3IQ77YBCi39e3BUY1svryixmdoFqpykbs8VIsyVVXaxC5rRYWBn4mfNG7C9HbOdqgTpSVsPHg8IQF0R00Ap1f2I1y');
 
 const CheckoutForm = () => {
     const stripe = useStripe();
